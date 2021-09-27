a7b9a362f0bbed6cb59b944bc1a4bf46f21a70ce
AnastasiaCymbalyuk <n.wolk@mail.ru>
2021-09-27 17:14:42 +0300

    задание 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,41 @@
+'use strict'
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  let D = b ** 2 - (4 * a * c);
+  let x1;
+	let x2;
+
+  if (D === 0) {
+    x1 = (- b / (2 * a));
+    arr = [x1];
+  } else if (D > 0) {
+    x1 = ((-b + Math.sqrt(D)) / (2 * a));
+    x2 = ((-b - Math.sqrt(D)) / (2 * a));
+    arr = [x1, x2];
+  } else {
+    arr = [];
+  };
+
+  return arr;
+};
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+  let arr = [{name: "Процентная ставка", value: percent},
+    {name: "Начальный взнос", value: contribution},
+    {name: "Общая стоимость", value: amount}];
 
-  // код для задачи №2 писать здесь
+    for (let i = 0; i < arr.length; i++) {
+	    if (isNaN(arr[i].value)) {
+		    return `Параметр "${arr[i].name}" содержит неправильное значение "${arr[i].value}"`;
+	    };
+    };
 
-  return totalAmount;
-}
+    let sum = amount - contribution;
+    let month = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+    let interestRate = (percent / 100) / 12;
+    let monthlyPayment = (sum * (interestRate + interestRate / (((1 + interestRate) ** month) - 1)));
+    let totalAmount = parseFloat((monthlyPayment * month).toFixed(2));
+
+    return totalAmount;
+};
