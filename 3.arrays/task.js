028157c0abac6956cf81dd7f4032e9757db27d71
AnastasiaCymbalyuk <n.wolk@mail.ru>
2021-09-29 11:11:54 +0300

    задание 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
-}
+  let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+  return result;
+};
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
-}
+  let resultArr = arr.filter((index) => index > 0 && index % 3 === 0).map((index) => index * 10);
+  return resultArr;
+};
