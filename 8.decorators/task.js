19809a05148ddafd4d484c04a089b5eeb8862d32
AnastasiaCymbalyuk <n.wolk@mail.ru>
2021-10-06 11:15:24 +0300

    задание 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,51 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+  let cash = [];
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let checkIndex = cash.findIndex((item) => item.hash === hash);
+    if (checkIndex !== -1) {
+      console.log(`Из кэша: ${cash[checkIndex].result}`);
+      return `Из кэша: ${cash[checkIndex].result}`;
+    } else {
+      let result = func( ...args);
+      cash.push({hash, result});
+      if (cash.length > 5) {
+        cash.shift();
+        console.log(`Удален элемент`);
+      };
+      console.log(`Вычисляем: ${result}`);
+      return `Вычисляем: ${result}`;
+    };
+  };
+  return wrapper;
+};
 
+function debounceDecoratorNew(func, ms) {
+  let flag = false;
+  function wrapper(...rest) {
+    if (!flag) {
+      func(...rest);
+      flag = true;
+      setTimeout(() => {
+        flag = false;
+      }, ms);
+    };
+  };
+  return wrapper;
+};
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
-
-function debounceDecorator2(func) {
-  // Ваш код
-}
+function debounceDecorator2(func, ms) {
+  let flag = false;
+  wrapper.count = 0;
+  function wrapper(...rest) {
+    wrapper.count++;
+    if (!flag) {
+      func(...rest);
+      flag = true;
+      setTimeout(() => {
+        flag = false;
+      }, ms);
+    };
+  };
+  return wrapper;
+};
